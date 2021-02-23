616f189b316e30531bb20a5cc2e46e7b1e2780c6
uy <695981841@qq.com>
2021-02-23 09:55:10 +0800

    NEW

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from client.nmapScan import Nmap_portscan
+from web import APP
 
-app = Flask(__name__)
-mongo = PyMongo(app, uri="mongodb://localhost:27017/test")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/home')
-def hello():
-    return '22'
-
-if __name__ == '__main__':
-    app.run()
--- web/route/api.py
+++ web/route/api.py
@@ -12,7 +12,6 @@ class UserLogin(Resource):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument("username", type=str, required=True, location='json')
         self.parser.add_argument("password", type=str, required=True, location='json')
-        self.parser.add_argument("rememberme", type=bool, location='json')
 
     def post(self):
         """登录接口"""
@@ -20,17 +19,13 @@ class UserLogin(Resource):
         key_username = args.username
         key_password = args.password
         key_remember = args.rememberme
-        try:  # 获取客户端IP地址
-            login_ip = request.headers['X-Forwarded-For'].split(',')[0]
-        except:
-            login_ip = request.remote_addr
+
         user_query = DB.db.users.find({}, {'username': key_username})
         if not user_query:  # 若不存在此用户
             return {'status_code': 201, 'msg': '用户名或密码错误'}
         if check_password_hash(user_query.password, key_password):  # 进行密码核对
             session['status'] = True  # 登录成功设置session
             session['username'] = key_username
-            session['login_ip'] = login_ip
             try:
                 DB.session.commit()
             except Exception as e:
